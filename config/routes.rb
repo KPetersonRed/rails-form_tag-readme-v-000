2852190eb992dfbb4232a0229ae39ea1a6966c61
Kristy Peterson <kpeterson.kap@gmail.com>
2018-03-09 17:23:37 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :new] #new creates the form we see to insert our data
+  resources :posts, only: [:index, :new, :create] #new creates the form we see to insert our data
+
+
 end
